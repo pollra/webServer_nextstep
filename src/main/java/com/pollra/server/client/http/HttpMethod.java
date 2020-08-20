b416d20b76006b03b8be00f26212288abf4cebbd
pollra <pollra32@naver.com>
2020-08-21 01:24:05 +0900

    리펙토링 : 코드 간략화

--- src/main/java/com/pollra/server/client/http/HttpMethod.java
+++ src/main/java/com/pollra/server/client/http/HttpMethod.java
@@ -1,14 +1,9 @@
 package com.pollra.server.client.http;
 
-import com.pollra.server.client.exception.MethodMissMathException;
-
 public enum HttpMethod {
     GET, POST, PUT, DELETE, HEAD, CONNECT, OPTIONS, TRACE, PATCH;
 
     public static HttpMethod match(String text) {
-        for (HttpMethod method : HttpMethod.values()) {
-            if(method.toString().equals(text)) return method;
-        }
-        throw new MethodMissMathException("존재하지 않는 메소드입니다");
+        return HttpMethod.valueOf(text.trim().toUpperCase());
     }
 }
